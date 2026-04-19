1b480fc8a06eba86c44018e7102aee124a99c16d
mahmoudsruby <mahmoudsruby@gmail.com>
2026-04-19 21:41:02 +0200

    Change the Website Icon , and Create Hearosection and change the page.js to works with the New Sections

--- app/page.js
+++ app/page.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 // import {Footer} from "@/components/Footer";
 import Header from './components/Header'; 
 import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen font-sans">
       <Header activeTab="الرئيسية" />
 
-      <main className="flex-grow bg-white">
+      <main className="flex-grow bg-white" dir="rtl">
+        <HeroSection />
       </main>
 
       <Footer />
